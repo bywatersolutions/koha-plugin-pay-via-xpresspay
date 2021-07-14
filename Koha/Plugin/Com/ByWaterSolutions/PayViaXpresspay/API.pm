b9c1082a0107d1fe5b9c439be822e23dc6493c50
Kyle M Hall <kyle@bywatersolutions.com>
2021-07-14 08:02:52 -0400

    Remove old code

--- Koha/Plugin/Com/ByWaterSolutions/PayViaXpresspay/API.pm
+++ Koha/Plugin/Com/ByWaterSolutions/PayViaXpresspay/API.pm
@@ -14,20 +14,9 @@ sub handle_payment {
     return $c->render( status => 204, text => q{} );
 
     if ( my %data = parse_urlencoded($body) ) {
-        my $twilio_status = $data{CallStatus};
 
-        warn "TWILIO: update_message_status(): " . Data::Dumper::Dumper( \%data );
+        warn "handle_payment: " . Data::Dumper::Dumper( \%data );
 
-        my $status = $twilio_status eq 'queued'      ? 'sent'   : # We should get another status update later
-                     $twilio_status eq 'ringing'     ? 'sent'   : # Ditto
-                     $twilio_status eq 'in-progress' ? 'sent'   : # The person picked up, basically completed
-                     $twilio_status eq 'completed'   ? 'sent'   : # Clearly completed
-                     $twilio_status eq 'busy'        ? 'failed' : # TODO: Make retrying busy a plugin setting
-                     $twilio_status eq 'failed'      ? 'failed' : # Phone number was most likely invalid
-                     $twilio_status eq 'no-answer'   ? 'failed' : # See TODO above
-                                                       'failed' ; # Staus was something we didn't expect
-        $message->status($status);
-        $message->store();
 
         return $c->render( status => 204, text => q{} );
     }
