6e877c672c15b874eb606b0dd4f6766c3ad484ae
Kyle Hall <kyle@bywatersolutions.com>
2021-08-02 17:49:23 +0000

    Implement postback api endpoint

--- Koha/Plugin/Com/ByWaterSolutions/PayViaXpresspay/API.pm
+++ Koha/Plugin/Com/ByWaterSolutions/PayViaXpresspay/API.pm
@@ -9,23 +9,61 @@ use WWW::Form::UrlEncoded qw(parse_urlencoded);
 sub handle_payment {
     my $c = shift->openapi->valid_input or return;
 
-    my $body = $c->req->body;
+    my $params = $c->req->params->to_hash;
 
-    return $c->render( status => 204, text => q{} );
+    my $borrowernumber   = $params->{l1};
+    my @accountlines_ids = split( /,/, $params->{l2} );
+    my $token            = $params->{l3};
+    my $amount = $params->{billAmount};
 
-    if ( my %data = parse_urlencoded($body) ) {
+    my $dbh      = C4::Context->dbh;
+    my $query    = "SELECT * FROM xpresspay_plugin_tokens WHERE token = ?";
+    my $token_hr = $dbh->selectrow_hashref( $query, undef, $token );
 
-        warn "handle_payment: " . Data::Dumper::Dumper( \%data );
+    return $c->render(
+        status  => 404,
+        openapi => { error => "Token not found" }
+    ) unless $token_hr;
 
+    my $patron = Koha::Patrons->find($borrowernumber);
 
-        return $c->render( status => 204, text => q{} );
-    }
-    else {
-        return $c->render(
-            status  => 500,
-            openapi => { error => "Unable to decode json" }
-        );
-    }
+    return $c->render(
+        status  => 404,
+        openapi => { error => "Patron not found" }
+    ) unless $patron;
+
+    my $account = $patron->account;
+
+    my $schema = Koha::Database->new->schema;
+
+    my @lines = Koha::Account::Lines->search(
+        { accountlines_id => { -in => \@accountlines_ids } } );
+    warn "ACCOUNTLINES TO PAY: ". Data::Dumper::Dumper( $_->unblessed ) for @lines;
+
+    my $payment = $schema->txn_do(
+        sub {
+            $dbh->do( "DELETE FROM xpresspay_plugin_tokens WHERE token = ?",
+                undef, $token );
+
+            return $account->pay(
+                {
+                    amount     => $amount,
+                    note       => 'Paid via Xpresspay',
+                    library_id => $patron->branchcode,
+                    lines      => \@lines,
+                }
+            );
+        }
+    );
+
+    warn "PAYMENT: " . Data::Dumper::Dumper( $payment );
+
+    return $c->render( status => 204, text => q{} ) if $payment->{payment_id};
+
+    return $c->render(
+        status  => 500,
+        openapi => { error => "Payment failed" }
+    );
 }
 
 1;
